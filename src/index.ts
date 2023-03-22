5fdd6a3907db511d1c1f3e2cb1d89640580e62a6
Kastriot <67861812+Kastriot78@users.noreply.github.com>
2023-03-22 12:33:57 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 const corsOptions = {
-    origin: 'http://localhost:3000'
+    origin: 'https://shop-now-nine.vercel.app'
 };
 
 app.use(cors(corsOptions));
