65241f8ea41ac900702b51fc86e6266c7bebc526
Kastriot <67861812+Kastriot78@users.noreply.github.com>
2023-03-23 14:49:39 +0100

    Update fileUpload.ts

--- fileUpload.ts
+++ fileUpload.ts
@@ -3,7 +3,7 @@ import multer from "multer";
 // Create the multer storage configuration
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "images/");
+      cb(null, "/images");
     },
     filename: function (req, file, cb) {
       cb(null, Date.now() + "-" + file.originalname);
