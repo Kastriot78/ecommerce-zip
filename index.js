e9e6acaafecdcc9e71833549c26361e9a23cb054
Kastriot <67861812+Kastriot78@users.noreply.github.com>
2023-03-23 14:45:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ app.get('/', (req, res) => {
     res.send('Welcome to Ecommerce APP.');
 });
 
-const directory = path.join(__dirname, 'images');
-app.use('/images', express.static(directory));
+const directory = path.join(__dirname, '/images');
+app.use('./images', express.static(directory));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
